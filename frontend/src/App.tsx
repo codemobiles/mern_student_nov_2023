e5f1de85e0fc6c2e3c39e4c1e74111442417d19c
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-11-06 11:24:59 +0700

    1234

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,15 @@
+import { Button } from "@mui/material";
 import React from "react";
 
 type Props = {};
 
 export default function App({}: Props) {
-  return <div>App</div>;
+  return (
+    <div>
+      App
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+    </div>
+  );
 }
