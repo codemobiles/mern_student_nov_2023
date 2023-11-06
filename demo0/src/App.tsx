fd8869f2e5e6a191e624493138a801f9e852e80c
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-11-06 10:58:39 +0700

    1234

--- demo0/src/App.tsx
+++ demo0/src/App.tsx
@@ -8,9 +8,15 @@ export default function App({}: Props) {
   const tmp3: string = "Lek";
   const tmp4: boolean = false;
 
+  function method1() {
+    console.log("Call me why?");
+  }
+
   return (
     <div>
       CodeMobiles {tmp1}, {tmp2}, {tmp3}, {tmp4 ? "yes" : "no"}
+      <br />
+      <button>Call me</button>
     </div>
   );
 }
