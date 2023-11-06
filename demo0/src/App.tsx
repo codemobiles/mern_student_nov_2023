da719846ffae5c347768ffba4ee33e1d3c1191b7
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-11-06 10:31:20 +0700

    1234

--- demo0/src/App.tsx
+++ demo0/src/App.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
-type Props = {}
+type Props = {};
 
 export default function App({}: Props) {
-  return (
-    <div>App</div>
-  )
+  return <div>CodeMobiles</div>;
 }
