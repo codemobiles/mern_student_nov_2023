c386380ec1201563bfcc070cd9ca75f03b0e6bd4
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-11-06 14:00:49 +0700

    1234

--- frontend/src/components/pages/LoginPage.tsx
+++ frontend/src/components/pages/LoginPage.tsx
@@ -3,5 +3,10 @@ import React from "react";
 type Props = {};
 
 export default function LoginPage({}: Props) {
-  return <div>LoginPage</div>;
+  return (
+    <div>
+      LoginPage
+      <img src="assets/images/react_js_logo.jpg" alt="" />
+    </div>
+  );
 }
