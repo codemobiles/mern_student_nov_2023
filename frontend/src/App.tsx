475fb1a3e31aa29ad196a6c3040385458fda85cc
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-11-06 11:59:33 +0700

    1234

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "./components/pages/LoginPage";
 import RegisterPage from "./components/pages/RegisterPage";
 
@@ -8,6 +8,7 @@ export default function App({}: Props) {
   return (
     <div>
       <Routes>
+        <Route path="" element={<Navigate to="/login" />} />
         <Route path="login" element={<LoginPage />} />
         <Route path="register" element={<RegisterPage />} />
       </Routes>
