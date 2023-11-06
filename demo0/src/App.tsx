78c2df6bda601e6497b4fb9321096f4da179ebf0
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-11-06 10:57:00 +0700

    1234

--- demo0/src/App.tsx
+++ demo0/src/App.tsx
@@ -3,7 +3,14 @@ import React from "react";
 type Props = {};
 
 export default function App({}: Props) {
-  let tmp = 0;
+  const tmp1 = 0;
+  const tmp2: number = 1;
+  const tmp3: string = "Lek";
+  const tmp4: boolean = false;
 
-  return <div>CodeMobiles {tmp}</div>;
+  return (
+    <div>
+      CodeMobiles {tmp1}, {tmp2}, {tmp3}, {tmp4 ? "yes" : "no"}
+    </div>
+  );
 }
