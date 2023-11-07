91a96166c85d9509b46de2c2cad0718d04653c88
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-11-07 15:05:27 +0700

    1234

---
+++ demoPOS/frontend/src/store/store.ts
@@ -0,0 +1,13 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { useDispatch } from "react-redux";
+
+const store = configureStore({
+  reducer: {},
+  devTools: import.meta.env.VITE_IS_PRODUCTION === "0", // show redux log in dev mode
+});
+
+// export type of root state from reducers
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export default store;
