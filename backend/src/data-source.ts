984d97f327bf59e039e6545150216b13341a8bf5
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-11-07 13:23:34 +0700

    1234

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -7,8 +7,9 @@ import { Counters } from "./entity/Counters";
 
 export const AppDataSource = new DataSource({
   type: "mongodb",
-  database: "demo",
-  port: 27018,
+  database: "demopos",
+  host: process.env.MONGO_HOST || "localhost",
+  port: Number(process.env.MONGO_PORT || 27018),
   synchronize: true,
   logging: false,
   entities: [Users, Products, Transactions, Counters],
