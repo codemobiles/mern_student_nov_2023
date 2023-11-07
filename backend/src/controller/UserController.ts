55215365eccf7f0ff3ea7d870f18c2f518d9204d
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-11-07 13:40:11 +0700

    1234

--- backend/src/controller/UserController.ts
+++ backend/src/controller/UserController.ts
@@ -7,4 +7,12 @@ export class UserController {
       return { result: "nok", message: "invalid data" };
     }
   }
+
+  async login(req: Request, res: Response, next: NextFunction) {
+    try {
+      return { result: "ok 1234", messsage: "login" };
+    } catch (e) {
+      return { result: "nok", message: "invalid data" };
+    }
+  }
 }
