d8f81333f411ffc6858dc50dd9f67edfb1e34ee9
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-11-07 13:24:34 +0700

    1234

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,37 +1,24 @@
 import * as express from "express";
-import * as bodyParser from "body-parser";
 import { Request, Response } from "express";
 import { AppDataSource } from "./data-source";
 import { Routes } from "./routes";
-import { User } from "./entity/User";
 
 AppDataSource.initialize()
   .then(async () => {
     // create express app
     const app = express();
-    app.use(bodyParser.json());
+    app.use(express.json());
 
     // register express routes from defined application routes
     Routes.forEach((route) => {
-      (app as any)[route.method](
-        route.route,
-        (req: Request, res: Response, next: Function) => {
-          const result = new (route.controller as any)()[route.action](
-            req,
-            res,
-            next
-          );
-          if (result instanceof Promise) {
-            result.then((result) =>
-              result !== null && result !== undefined
-                ? res.send(result)
-                : undefined
-            );
-          } else if (result !== null && result !== undefined) {
-            res.json(result);
-          }
+      (app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
+        const result = new (route.controller as any)()[route.action](req, res, next);
+        if (result instanceof Promise) {
+          result.then((result) => (result !== null && result !== undefined ? res.send(result) : undefined));
+        } else if (result !== null && result !== undefined) {
+          res.json(result);
         }
-      );
+      });
     });
 
     // setup express app here
@@ -41,8 +28,6 @@ AppDataSource.initialize()
     let port = process.env.PORT || 8081;
     app.listen(port);
 
-    console.log(
-      `Express server has started on port ${port}. Open http://localhost:3000/users to see results`
-    );
+    console.log(`Express server has started on port ${port}. Open http://localhost:3000/users to see results`);
   })
   .catch((error) => console.log(error));
