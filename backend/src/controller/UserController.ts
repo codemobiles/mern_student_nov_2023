e7c5c687b3da5ef1b847791995ab5895fb308cea
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-11-07 13:50:03 +0700

    1234

--- backend/src/controller/UserController.ts
+++ backend/src/controller/UserController.ts
@@ -1,8 +1,20 @@
 import { NextFunction, Request, Response } from "express";
+import { AppDataSource } from "../data-source";
+import { Users } from "../entity/Users";
+import * as bcrypt from "bcryptjs";
+
 export class UserController {
+  private userRepository = AppDataSource.getMongoRepository(Users);
+
   async register(req: Request, res: Response, next: NextFunction) {
     try {
-      return { result: "ok 1234", messsage: "register" };
+      // req.body.created = savedValue(req.body.created, new Date());
+      // req.body.level = savedValue(req.body.level, "normal");
+      // req.body.__v = savedValue(req.body.__v, 0);
+
+      req.body.password = await bcrypt.hash(req.body.password, 8);
+      const doc = await this.userRepository.save(req.body);
+      return { result: "ok", message: doc };
     } catch (e) {
       return { result: "nok", message: "invalid data" };
     }
