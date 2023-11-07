4572706fd46f29f7597029a52433f4f98f23cdfe
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-11-07 13:21:07 +0700

    1234

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -1,6 +1,9 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
-import { User } from "./entity/User";
+import { Users } from "./entity/Users";
+import { Products } from "./entity/Products";
+import { Transactions } from "./entity/Transactions";
+import { Counters } from "./entity/Counters";
 
 export const AppDataSource = new DataSource({
   type: "mongodb",
@@ -8,7 +11,7 @@ export const AppDataSource = new DataSource({
   port: 27018,
   synchronize: true,
   logging: false,
-  entities: [User],
+  entities: [Users, Products, Transactions, Counters],
   migrations: [],
   subscribers: [],
 });
