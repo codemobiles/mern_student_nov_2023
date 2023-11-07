ea218886524353e3b3aec30c5a8d5efe140c8139
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-11-07 14:01:32 +0700

    1234

--- backend/src/controller/UserController.ts
+++ backend/src/controller/UserController.ts
@@ -2,15 +2,16 @@ import { NextFunction, Request, Response } from "express";
 import { AppDataSource } from "../data-source";
 import { Users } from "../entity/Users";
 import * as bcrypt from "bcryptjs";
+import { savedValue } from "../utils/cm-util";
 
 export class UserController {
   private userRepository = AppDataSource.getMongoRepository(Users);
 
   async register(req: Request, res: Response, next: NextFunction) {
     try {
-      // req.body.created = savedValue(req.body.created, new Date());
-      // req.body.level = savedValue(req.body.level, "normal");
-      // req.body.__v = savedValue(req.body.__v, 0);
+      req.body.created = savedValue(req.body.created, new Date());
+      req.body.level = savedValue(req.body.level, "normal");
+      req.body.__v = savedValue(req.body.__v, 0);
 
       req.body.password = await bcrypt.hash(req.body.password, 8);
       const doc = await this.userRepository.save(req.body);
