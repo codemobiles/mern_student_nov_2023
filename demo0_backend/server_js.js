291987a527b092530a968788b3f2ce97c7cde287
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-11-07 10:24:27 +0700

    1234

--- demo0_backend/server_js.js
+++ demo0_backend/server_js.js
@@ -5,4 +5,8 @@ app.get("/", (req, res) => {
   res.json({ result: "ok" });
 });
 
+app.get("/login", (req, res) => {
+  res.json({ result: "login", echo: req.query });
+});
+
 app.listen(8085, () => console.log("Server is  running..."));
