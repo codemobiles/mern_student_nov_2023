b83c8a7f5d4e87901c7a29b70a2491e64b23beb2
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-11-08 13:22:00 +0700

    1234

--- demoPOS/frontend/src/components/pages/StockPage.tsx
+++ demoPOS/frontend/src/components/pages/StockPage.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+import { getProducts, stockSelector } from "@/store/slices/stockSlice";
+import { useAppDispatch } from "@/store/store";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
 
 type Props = {};
 
 export default function StockPage({}: Props) {
+  const stockReducer = useSelector(stockSelector);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(getProducts());
+  }, []);
+
   return <div>StockPage</div>;
 }
