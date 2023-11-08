6d5a828408dccad09348e6c3938e4f8c6df66601
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-11-08 14:50:00 +0700

    1234

--- demoPOS/frontend/src/store/slices/stockSlice.ts
+++ demoPOS/frontend/src/store/slices/stockSlice.ts
@@ -14,6 +14,7 @@ const initialState: StockState = {
   stockOneResult: null,
 };
 
+// Query
 export const getProducts = createAsyncThunk(
   "stock/getProducts",
   async (keyword: string | undefined) => {
@@ -26,6 +27,39 @@ export const getProducts = createAsyncThunk(
       const result = await httpClient.get<Product[]>(server.PRODUCT_URL);
       return result.data;
     }
+  }
+);
+
+// Add
+export const addProduct = createAsyncThunk(
+  "stock/add",
+  async (formData: FormData) => {
+    await httpClient.post(server.PRODUCT_URL, formData);
+  }
+);
+
+// Query by Id
+export const getProductById = createAsyncThunk(
+  "stock/getById",
+  async (id: string): Promise<Product> => {
+    const result = await await httpClient.get(`${server.PRODUCT_URL}/id/${id}`);
+    return result.data;
+  }
+);
+
+// Delete
+export const deleteProduct = createAsyncThunk(
+  "stock/delete",
+  async (id: string) => {
+    await httpClient.delete(`${server.PRODUCT_URL}/id/${id}`);
+  }
+);
+
+// Edit
+export const editProduct = createAsyncThunk(
+  "stock/edit",
+  async (formData: any) => {
+    await httpClient.put(server.PRODUCT_URL, formData);
   }
 );
 
