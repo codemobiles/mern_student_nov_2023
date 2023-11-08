55778e3465f5c38df481df6c3d36f08a3f7ea00c
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-11-08 11:53:30 +0700

    1234

--- demoPOS/backend/src/controller/ProductController.ts
+++ demoPOS/backend/src/controller/ProductController.ts
@@ -1,3 +1,9 @@
 import { NextFunction, Request, Response } from "express";
+import { Products } from "../entity/Products";
+import { AppDataSource } from "../data-source";
 
-export class ProductController {}
+export class ProductController {
+    private productRepository = AppDataSource.getMongoRepository(Products);
+    
+    
+}
