754b43ccf1b9f6a2d38ba16c1f7292af2f5b7f52
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-11-08 15:09:30 +0700

    1234

--- demoPOS/frontend/src/main.tsx
+++ demoPOS/frontend/src/main.tsx
@@ -5,12 +5,15 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store/store.ts";
+import InjectTailwind from "./InjectTailwind.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-        <App />
+        <InjectTailwind>
+          <App />
+        </InjectTailwind>
       </Provider>
     </BrowserRouter>
   </React.StrictMode>
