89d1f88636eb500c09723294aff00feb47a01325
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-11-09 11:19:56 +0700

    1234

--- demoPOS/backend/src/index.ts
+++ demoPOS/backend/src/index.ts
@@ -27,7 +27,8 @@ AppDataSource.initialize()
       (app as any)[route.method](
         "/api/v2" + route.route,
         (req: Request, res: Response, next: Function) => {
-          next();
+          // next();
+          res.end("no authoized");
         },
         (req: Request, res: Response, next: Function) => {
           const result = new (route.controller as any)()[route.action](
