8f479daa5ad1183bc9f402266cce696b04c65157
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-11-09 10:12:39 +0700

    1234

--- demoPOS/frontend/src/hooks/useCopyToClipboard.ts
+++ demoPOS/frontend/src/hooks/useCopyToClipboard.ts
@@ -2,6 +2,8 @@ import React, { useEffect } from "react";
 import copy from "copy-to-clipboard";
 
 export default function useCopyToClipboard(resetInterval: number) {
-  const handleCopy = (text: string) => {};
+  const handleCopy = (text: string) => {
+    copy(text);
+  };
   return [true, handleCopy];
 }
